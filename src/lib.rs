31b94cb998374f2a9d8161f82e5f01a58db23ae7
i5heu <i5h@i5h.eu>
2020-06-24 20:35:40 +0200

    prepare test for sqlite return

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,38 @@
+use rusqlite::NO_PARAMS;
+use rusqlite::{Connection, Result};
+
+pub struct Foo {
+    db_name: String,
+    conn: rusqlite::Connection,
+}
+
+pub fn init() -> Result<Foo> {
+    let conn = Connection::open("cats.db")?;
+
+    conn.execute(
+        "create table if not exists cat_colors (
+             id integer primary key,
+             name text not null unique
+         )",
+        NO_PARAMS,
+    )?;
+    conn.execute(
+        "create table if not exists cats (
+             id integer primary key,
+             name text not null,
+             color_id integer not null references cat_colors(id)
+         )",
+        NO_PARAMS,
+    )?;
+
+    let bar = Foo {
+        db_name: "Hello".to_owned(),
+        conn,
+    };
+    // return bar;
+    Ok(bar)
+}
+
 pub mod math {
     pub const fn add(a: i8, b: i8) -> i8 {
         return a + b;
