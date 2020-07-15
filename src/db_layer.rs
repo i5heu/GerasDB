361a35cc648f5525cc66abe95a08d906fc82e480
i5heu <i5h@i5h.eu>
2020-07-15 21:05:12 +0200

    working insert and get

--- src/db_layer.rs
+++ src/db_layer.rs
@@ -1,17 +1,139 @@
+use rusqlite::{params, Connection, Result, NO_PARAMS};
+use std::{process, sync::Arc};
+
 pub struct PersistentItem {
-    hash: SHA3,
-    treeHash: SHA3, // random hash defined on the root of the tree,
-    parentHash: SHA3,
-    lvl: int, //the level above the tree root,
-    creator: SHA3, // the creator of this link for diff between sys and usr,
-    created: timestamp, // with ms,
-    importance: int, // the importance of the data - higher is more important - will be used to decide if unimportant data will be sacrificed for the sake of the reliability of more important data
-    content: blob,
-    deleted: bool,
-    hashIfDeleted: SHA3,
+    pub hash: String,
+    pub tree_hash: String, // random hash defined on the root of the tree,
+    pub parent_hash: String,
+    pub lvl: u32, //the level above the tree root,
+    pub creator: String, // the creator of this link for diff between sys and usr,
+    pub created: i64, // with ms,
+    pub importance: u32, // the importance of the data - higher is more important - will be used to decide if unimportant data will be sacrificed for the sake of the reliability of more important data
+    pub content: String,
+    pub deleted: bool,
+    pub hash_if_deleted: String,
+    pub last_checked: i64, // with ms,
+    pub reading_errors: u32,
+    pub extras: String, // json
+}
 
 
-    lastChecked: timestamp, // with ms,
-    readingErrors: int,
-    extras: string, // json
+pub fn insert(conn: &Arc<Connection>, item: &PersistentItem) -> Result<(), rusqlite::Error> {
+    conn.execute(
+        "INSERT INTO persistentStore (
+            hash,
+            tree_hash,
+            parent_hash,
+            lvl,
+            creator,
+            created,
+            importance,
+            content,
+            deleted,
+            hash_if_deleted,
+            last_checked,
+            reading_errors,
+            extras
+        ) VALUES (
+        ?,
+        ?,
+        ?,
+        ?,
+        ?,
+        ?,
+        ?,
+        ?,
+        ?,
+        ?,
+        ?,
+        ?,
+        ?
+      );",
+      params![
+        item.hash,
+        item.tree_hash,
+        item.parent_hash,
+        item.lvl,
+        item.creator,
+        item.created,
+        item.importance,
+        item.content,
+        item.deleted,
+        item.hash_if_deleted,
+        item.last_checked,
+        item.reading_errors,
+        item.extras
+      ] 
+    )?;
+
+
+    Ok(())
+}
+
+
+pub fn get(conn: &Arc<Connection>, hash: &String) -> Result<PersistentItem, rusqlite::Error> {
+    let mut stmt = conn.prepare("SELECT 
+            hash,
+            tree_hash,
+            parent_hash,
+            lvl,
+            creator,
+            created,
+            importance,
+            content,
+            deleted,
+            hash_if_deleted,
+            last_checked,
+            reading_errors,
+            extras
+        FROM persistentStore WHERE hash = :search_hash")?;
+
+    let hash_iter = stmt.query_map(params![hash], |row| Ok(PersistentItem { 
+        hash: row.get(0)?,
+        tree_hash: row.get(1)?,
+        parent_hash: row.get(2)?,
+        lvl: row.get(3)?,
+        creator: row.get(4)?,
+        created: row.get(5)?,
+        importance: row.get(6)?,
+        content: row.get(7)?,
+        deleted: row.get(8)?,
+        hash_if_deleted: row.get(9)?,
+        last_checked: row.get(10)?,
+        reading_errors: row.get(11)?,
+        extras: row.get(12)?,
+    }))?;
+
+
+    let mut foo:Option<PersistentItem> = None;
+
+    for hash in hash_iter {
+        foo = Some( match hash {
+            Ok(e) => e,
+            Err(e) => {
+                eprintln!("{}", e);
+                process::exit(1);
+            }
+        });
+    }
+    
+    if let Some(x) = foo {
+        Ok(x)
+    } else {
+        Ok(PersistentItem{
+            hash: String::from("NOT FOUND"),
+            tree_hash: String::from(""),
+            parent_hash: String::from(""),
+            hash_if_deleted: String::from(""),
+            lvl: 0,
+            creator: String::from(""),
+            created: 0,
+            importance: 0,
+            content: String::from(""),
+            deleted: false,
+            last_checked: 0,
+            reading_errors: 0,
+            extras: String::from(""),
+        })
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,10 @@
 pub use rusqlite::Error as dbError;
 use rusqlite::{params, Connection, Result, NO_PARAMS};
 use std::{process, sync::Arc};
+use db_layer::PersistentItem;
 mod initialize_db;
+mod db_layer;
+
 pub struct Foo {
     pub db_name: String,
     pub conn: Arc<rusqlite::Connection>,
@@ -27,45 +30,35 @@ pub fn init() -> Result<Foo, rusqlite::Error> {
 }
 
 #[test]
-fn initialization_test() -> Result<(), rusqlite::Error> {
+fn set_and_get_test() -> Result<(), rusqlite::Error> {
     let result = init()?;
 
-    result.conn.execute(
-        "INSERT INTO persistentStore (hash,treeHash,parentHash,hashIfDeleted,level,creator,created,importance,deleted,lastChecked,readingErrors,extras) VALUES (
-        '83bff28dde1b1bf5810071c6643c08e5b05bdb836effd70b403ea8ea0a634dc4997eb1053aa3593f590f9c63630dd90b',
-        '83bff28dde1b1bf5810071c6643c08e5b05bdb836effd70b403ea8ea0a634dc4997eb1053aa3593f590f9c63630dd90b',
-        '83bff28dde1b1bf5810071c6643c08e5b05bdb836effd70b403ea8ea0a634dc4997eb1053aa3593f590f9c63630dd90b',
-        '83bff28dde1b1bf5810071c6643c08e5b05bdb836effd70b403ea8ea0a634dc4997eb1053aa3593f590f9c63630dd90b',
-        '0',
-        '83bff28dde1b1bf5810071c6643c08e5b05bdb836effd70b403ea8ea0a634dc4997eb1053aa3593f590f9c63630dd90b',
-        '1594755983',
-        '24124',
-        'false',
-        '1594755983',
-        'LOREM iPsUM!',
-        '{\"test\":\"hello world\"}'
-      );",
-      NO_PARAMS 
-    )?;
+    let hash = "83bff28dde1b1bf5810071c6643c08e5b05bdb836effd70b403ea8ea0a634dc4997eb1053aa3593f590f9c63630dd90b";
+    
+    let test_item: &PersistentItem = &PersistentItem{
+        hash: String::from(hash),
+        tree_hash: String::from(hash),
+        parent_hash: String::from(hash),
+        hash_if_deleted: String::from(hash),
+        lvl: 456835687,
+        creator: String::from(hash),
+        created: 567445672,
+        importance: 234235675,
+        content: String::from(hash),
+        deleted: false,
+        last_checked: 2141235,
+        reading_errors: 235235,
+        extras: String::from(hash),
+    };
+    
+    let _ = db_layer::insert(&result.conn, &test_item)?;
 
-    struct TestHash {
-        hash: String,
-    }
+    let get_result = db_layer::get(&result.conn, &test_item.hash)?;
 
-    let mut stmt = result.conn.prepare("SELECT hash FROM persistentStore")?;
-    let hash_iter = stmt.query_map(params![], |row| Ok(TestHash { hash: row.get(0)? }))?;
-
-    for hash in hash_iter {
-        let boo = match hash {
-            Ok(e) => e,
-            Err(e) => {
-                eprintln!("{}", e);
-                process::exit(1);
-            }
-        };
-
-        assert_eq!(boo.hash, "83bff28dde1b1bf5810071c6643c08e5b05bdb836effd70b403ea8ea0a634dc4997eb1053aa3593f590f9c63630dd90b")
-    }
+    assert_eq!(hash, get_result.hash);
+    assert_eq!(hash, get_result.content);
+    assert_eq!(2141235, get_result.last_checked);
+    assert_eq!(hash, get_result.extras);
 
     Ok(())
 }
